b9de2cf45bb64561be6ebd5281efd1bb2c0a1a99
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-06-25 09:06:53 -0500

    buildroot confignstuff

---
+++ linux/do_build.sh
@@ -0,0 +1,7 @@
+curl -L https://github.com/buildroot/buildroot/archive/refs/tags/2025.02.4.tar.gz -o buildroot.tar.gz
+tar -xf buildroot.tar.gz && mv buildroot*/ buildroot/
+# build linux (this should take a long time)
+make -C buildroot BR2_EXTERNAL=$(realpath extern) rv_defconfig 
+make -C buildroot 
+# build linux once more to fix initrd issues (should not take long)
+make -C buildroot linux-rebuild opensbi-rebuild all
